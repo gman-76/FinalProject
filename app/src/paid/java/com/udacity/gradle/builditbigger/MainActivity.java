cdc1a0b390be4e8f8d6242e04170203a2c70d702
gman-76 <gman.76@hotmail.com>
2015-11-10 12:03:52 +0000

    resolved resource id bug

--- app/src/paid/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
 
 import com.projects.gerhardschoeman.jokeactivity.MainJokeActivity;
 import com.projects.gerhardschoeman.jokeactivity.MainJokeActivityFragment;
